b527ab4e6f5aefe359fa6cc1b4ac4afd419db200
SantiagoGaona <sgaonacarvajal@outlook.com>
2023-09-19 15:39:41 -0500

    feat: create new products

--- api/models/data/json.data.go
+++ api/models/data/json.data.go
@@ -61,6 +61,66 @@ const ProductsJSON = `
     "productName": "iPhone X",
     "rating": 4.3,
     "imageUrl": "https://cdn.alloallo.media/catalog/product/apple/iphone/iphone-x/iphone-x-space-gray.jpg"
+  },
+  {
+    "vendor": "AT&T",
+    "productName": "iPhone 8",
+    "rating": 4.2,
+    "imageUrl": "https://www.att.com/scmsassets/global/devices/phones/apple/iphone-8/defaultimage/apple-iphone-8-silver.png"
+  },
+  {
+    "vendor": "Verizon",
+    "productName": "iPhone 7",
+    "rating": 4.1,
+    "imageUrl": "https://refurbi.com.co/cdn/shop/products/8-sgpng_500x.png?v=1694720818"
+  },
+  {
+    "vendor": "Samsung",
+    "productName": "Galaxy S21 Ultra 5G",
+    "rating": 4.1,
+    "imageUrl": "https://m.media-amazon.com/images/I/71bdE2zv5+S.jpg"
+  },
+  {
+    "vendor": "LG",
+    "productName": "LG Velvet 5G",
+    "rating": 3.2,
+    "imageUrl": "https://m.media-amazon.com/images/I/61eWYz4G0yL.jpg"
+  },
+  {
+    "vendor": "Motorola",
+    "productName": "Motorola Edge",
+    "rating": 3.1,
+    "imageUrl": "https://compras.tigo.com.co/arquivos/ids/161706-1000-1000/Edge-30-Fusion_Black_2000x2000px_1.png?v=638018609891300000"
+  },
+  {
+    "vendor": "Huawei",
+    "productName": "Huawei P40 Pro",
+    "rating": 4.6,
+    "imageUrl": "https://img01.huaweifile.com/sg/ms/co/pms/uomcdn/CO_HW_B2C/pms/202304/gbom/6941487295333/800_800_2DCB72F6EB52010812BE0B3537349472mp.png"
+  },
+  {
+    "vendor": "Xiaomi",
+    "productName": "Xiaomi Mi 11",
+    "rating": 4.1,
+    "imageUrl": "https://www.movilshopcolombia.com/wp-content/uploads/2021/12/mi-11-lite-Verde.png"
+  },
+  {
+    "vendor": "Sony",
+    "productName": "Sony Xperia 1 III",
+    "rating": 3.1,
+    "imageUrl": "https://co.celulares.com/fotos/sony-xperia-1-iii-90987-g-alt.jpg"
+  },
+  {
+    "vendor": "OnePlus",
+    "productName": "OnePlus 9 Pro",
+    "rating": 4.2,
+    "imageUrl": "https://co.celulares.com/fotos/oneplus-9-pro-90470-g-alt.jpg"
+  },
+  {
+    "vendor": "Google",
+    "productName": "Google Pixel 5",
+    "rating": 3.2,
+    "imageUrl": "https://co.celulares.com/fotos/google-pixel-5-87666-g-alt.jpeg"
   }
 ]
 `
